d3b29eb003c93bb2c65acd733a5a53a7fa458bcf
Cisco Muratalla <ciscomuratalla@gmail.com>
2018-11-15 10:45:08 -0800

    added subplots
    4 charts for subplots

--- gen_test_report.py
+++ gen_test_report.py
@@ -49,12 +49,47 @@ path = os.getcwd()
 
 df = pd.concat([pd.read_csv(f) for f in glob.glob(path + '/*.csv')])
 
-df_plot = df.pivot_table('Execution_time', index = 'Facility', columns = 'Description')
 
+# convert int to str in 'Result'
+df['Result'] = df['Result'].apply(lambda x: 'Fail' if 0 <= x <= 5 else 'Success')
+
+# Chart 1
+df_plot = df.pivot_table('Execution_time', index = 'Facility', columns = 'Description')
 df_plot.plot(kind='bar')
+plt.title('Animal Speed by Facility')
 plt.ylabel('seconds')
+plt.tight_layout()
+
+# Chart 2
+df_avg_speed = df['Execution_time'].groupby(df['Description']).mean().sort_values(ascending=False)
+df_avg_speed.plot(kind='barh')
+plt.title('Animal Mean Speed')
+plt.xlabel('seconds')
+plt.tight_layout()
+
+# Chart 3
+animal_results = pd.crosstab(df['Result'],df['Description']).apply(lambda r: r/r.sum(), axis=0)
+animal_results.unstack(level=0).plot(kind='bar', stacked=True)
+
+# Chart 4
+df_info = df.pivot_table('Execution_time', index = 'Facility', columns = 'Description')
+
+# subplot
+fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(10,10))
+df_plot.plot(kind='bar', ax=ax1)
+df_avg_speed.plot(kind='barh', ax=ax2)
+animal_results.unstack(level=0).plot(kind='bar', stacked=True, ax=ax3)
+df_info.plot(ax=ax4)
 plt.tight_layout()
 
 plt.savefig(os.fspath('stats.png'))
 
-os.startfile(Path('stats.png'))
+# open file in either Windows or Mac
+def open_file():
+    file = 'stats.png'
+    if os.name == 'nt':
+        os.startfile(Path(file))
+    else:
+        os.system('start '+ (Path + file))
+
+open_file()
